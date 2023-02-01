15ee6945a7ac53715b29dfc9b3029c4c13ac08a4
dombrovskin <natashabahalika@gmail.com>
2023-02-01 18:16:24 +0200

    first commit

---
+++ lesson1/main.js
@@ -0,0 +1,42 @@
+/*Створити змінні*/
+let a = 'hello';
+console.log(a)
+let b = 'owu';
+console.log(b)
+let c = 'com';
+console.log(c)
+let d = 1;
+console.log(d)
+let e = 10;
+console.log(e)
+let f = -999;
+console.log(f)
+const PI = 3.14;
+console.log(PI)
+const E = 2.7;
+console.log(E)
+let g = 16;
+console.log(g)
+let qwe = true;
+console.log(qwe)
+let ewq = false;
+console.log(ewq)
+
+/*Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ.
+З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)*/
+
+let greeting = 'Nataliya';
+let intro = 'Valerivna';
+let surname = 'Dombrovska';
+let msg = `${greeting} ${intro} ${surname}`;
+console.log(msg)
+
+/* За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+  let a = 100; let b = '100'; let c = true;*/
+
+let l = 100;
+console.log(typeof l);
+let o ='100';
+console.log(typeof o)
+let k = true;
+console.log(k)
